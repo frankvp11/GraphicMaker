26751ed27cc73f10d40a1cc1bae441010e0bcb2b
frankvp11 <frankvanpaassen3@gmail.com>
2024-04-09 10:38:13 -0400

    made main.py global updating

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import math
-from nicegui import ui
+from nicegui import ui, app
 from svg import SVG
 from Shapes.AlphaMask.alphamask import AlphaMask
 from Shapes.Circle.circle import Circle
@@ -13,17 +13,21 @@ from Shapes.Text.text import Text
 from Shapes.Clip.clip import Clip
 from Shapes.Group.group import Group
 
-svg=  SVG().style("border: 1px solid black;")
-
-with svg:
-    group = Group()
-    with group:
-        Circle(50, 50, 20, fill="red")
-        Rectangle(100, 100, 50, 50, fill="green")
-        Line(150, 150, 200, 200, stroke="blue", stroke_width=5)
-        NGon(250, 250, 5, 50, fill="yellow")
-    # group.move_group(20, 0)
-    Rectangle(0, 0, 20, 20, fill="black").on("svg:pointerdown", lambda: group.move_group(20, 0))
 
 
-ui.run()
+def main():
+    svg=  SVG().style("border: 1px solid black;")
+
+
+    with svg:
+        group = Group()
+        with group:
+            Circle(50, 50, 20, fill="red")
+            Rectangle(100, 100, 50, 50, fill="green")
+            Line(150, 150, 200, 200, stroke="blue", stroke_width=5)
+            NGon(250, 250, 5, 50, fill="yellow")
+        Rectangle(0, 0, 20, 20, fill="black").on("svg:pointerdown", lambda: group.move_group(20, 0))
+
+
+main()
+ui.run(on_air=True)
