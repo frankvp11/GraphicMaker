162de4ba3543b8a11f5d279249e101fe1cdcb379
frankvp11 <frankvanpaassen3@gmail.com>
2024-04-30 14:00:37 -0400

    Refactor SVG components to add viewBox prop

--- svg.js
+++ svg.js
@@ -1,11 +1,12 @@
 export default {
   template: `
-    <svg :viewBox="'0 0 ' + width + ' ' + height" :width="width" :height="height" xmlns="http://www.w3.org/2000/svg">
+    <svg :viewBox="viewBox" :width="width" :height="height" xmlns="http://www.w3.org/2000/svg">
       <slot></slot>
     </svg>
   `,
   props: {
     width: { type: Number, default: 200 },
     height: { type: Number, default: 200 },
+    viewBox: { type: String, default: "0 0 200 200" },
   },
 };
--- svg.py
+++ svg.py
@@ -4,14 +4,14 @@ from nicegui.element import Element
 
 
 class SVG(Element, component="svg.js"):
-    def __init__(self, width: int = 200, height: int = 200) -> None:
+    def __init__(self, width: int = 200, height: int = 200, viewBox : dict = {"x":0, "y":0, "width":200, "height":200}) -> None:
         super().__init__()
         self.width = width
         self.height = height
         self.children = []
-
         self._props['width'] = width
         self._props['height'] = height
+        self._props['viewBox'] = " ".join(([str(val) for val in viewBox.values()]))
     
     def add_child(self, child):
         self.children.append(child)
